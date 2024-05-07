e161dbf05fbf3b5230af911bea29467989ea475a
Stian Sundby <stiansundbyy@gmail.com>
2024-05-07 10:20:27 +0200

    Løsningsforslag
    La til føsningsforslag til SASS oppgaven under Frontend

---
+++ Frontend/SASS/script.js
@@ -0,0 +1,18 @@
+const carouselItems = document.querySelectorAll(".carousel_image");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+let i = 1;
+
+setInterval(() => {
+	Array.from(carouselItems).forEach((item, index) => {
+		if (i < carouselItems.length) {
+			item.style.transform = `translateX(-${i * 100}%)`;
+		}
+	});
+
+	if (i < carouselItems.length) {
+		i++;
+	} else {
+		i = 0;
+	}
+}, 2000);
